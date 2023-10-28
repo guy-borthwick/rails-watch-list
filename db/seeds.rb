6894069f0282e44ffa4e21105f95cb242e0b846c
Guy Borthwick <guyborthwick@yahoo.com>
2023-10-28 15:45:53 +0100

    Day 1 of rails watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+serialized_movies = URI.open(url).read
+movies_data = JSON.parse(serialized_movies)
+
+puts "Seeding movies..."
+
+movies_data['results'].each do |movie_hash|
+  title = movie_hash['title']
+  overview = movie_hash['overview']
+  poster_url = "https://image.tmdb.org/t/p/w500/#{movie_hash['poster_path']}"
+  rating = movie_hash['vote_average']
+
+  Movie.create!(
+    title: title,
+    overview: overview,
+    poster_url: poster_url,
+    rating: rating
+  )
+end
+
+puts "Finished seeding!"
